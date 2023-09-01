fd6909ccdf936655da3ddb430f8ce2d2cbfe0f61
eseh1894 <134386707+eseh1894@users.noreply.github.com>
2023-09-01 11:33:06 +0200

    Add files via upload

--- RecipeListPage.jsx
+++ RecipeListPage.jsx
@@ -1,9 +1,13 @@
 import { Center, Heading } from "@chakra-ui/react";
 import { data } from "../utils/data";
+import RecipeCard from "../components/ui/RecipeCard";
 
-export const RecipeListPage = ({ recipe }) => {
+export const RecipeListPage = ({ clickFn }) => {
   return (
     <Center h="100vh" bgColor={"blackAlpha.700"} flexDir="column">
+      {data.hits.map((recipe) => (
+        <RecipeCard clickFn={clickFn} item={recipe} key={recipe.label} />
+      ))}
       <Heading>Your Recipe App</Heading>
     </Center>
   );
